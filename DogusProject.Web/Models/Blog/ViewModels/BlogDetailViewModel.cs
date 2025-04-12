4735281cc76d5edefc7189965131fea7366b3ffc
Serhat Tomak <serhattomak@gmail.com>
2025-04-13 00:28:57 +0300

    style: changes on button styles, category tabs

--- DogusProject.Web/Models/Blog/ViewModels/BlogDetailViewModel.cs
+++ DogusProject.Web/Models/Blog/ViewModels/BlogDetailViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using DogusProject.Application.Features.Blogs.Dtos;
 using DogusProject.Web.Models.Comment.DTOs;
+using DogusProject.Web.Models.Tag.DTOs;
 
 namespace DogusProject.Web.Models.Blog.ViewModels;
 
@@ -8,4 +9,5 @@ public class BlogDetailViewModel
 	public BlogDetailDto Blog { get; set; } = null!;
 	public List<CommentResponseDto> Comments { get; set; } = new();
 	public CreateCommentDto NewComment { get; set; } = new();
+	public TagDto Tag { get; set; } = new();
 }
